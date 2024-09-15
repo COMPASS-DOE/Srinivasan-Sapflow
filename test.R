74b33ae6e1fb63d90a645386e83cf2a566f62812
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2024-09-15 19:15:07 -0400

    test_baci
    use baci code for sample data

---
+++ test.R
@@ -0,0 +1,25 @@
+
+set.seed(123)
+nochange <- function() rnorm(50, mean = 1, sd = 0.25)
+change <- function() rnorm(50, mean = 1.25, sd = 0.25)
+
+library(dplyr)
+library(car)
+
+baci <- bind_rows(
+  tibble(case = "control_B", year = 1951:2000, value = nochange()),
+  tibble(case = "impact_B", year = 1951:2000, value = nochange()),
+  tibble(case = "control_A", year = 2001:2050, value = nochange()),
+  #tibble(case = "+t", x_control = nochange(), x_treatment = change()),
+  tibble(case = "impact_A", year = 2001:2050, value = change())
+)
+
+baci %>%
+  separate(case, into = c("CI", "BA"), sep = "_") -> baci2
+
+
+ggplot(baci2, aes(year, value, color = CI, shape = BA)) + geom_point() 
+
+test_baci <- glmer(value ~ BA + CI + BA*CI + (1|year),
+                   data = baci2, family = gaussian)
+Anova(test_baci, type = "III")  
