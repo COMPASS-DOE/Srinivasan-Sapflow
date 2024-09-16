e59d0627ce3855e6c9cee64e88420b60c62c43f4
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2024-09-16 17:53:42 -0400

    getting there

--- TMP_BACI.R
+++ TMP_BACI.R
@@ -1,4 +1,5 @@
-#Using GLMM BACI Analysis method from Pardini et. al. (2018) to analyze TEMPEST sapflow data 
+#Using GLMM BACI Analysis method from Pardini et. al. (2018)
+#as an initial template to analyze TEMPEST sapflow data 
 
 library(tidyr)
 library(readr)
@@ -20,117 +21,149 @@ sapply(.packages, require, character.only=TRUE)
 #read in TEMPEST sapflow data
 #tmp_full <- readRDS("Full_21_24.rds")
 
+full_data %>%
+  filter(month(TIMESTAMP) %in% c(5,6,7)) %>%
+  group_by(ID, date(TIMESTAMP)) %>%
+  mutate(peak = max(`F`)) %>%
+  ggplot(aes(hour(TIMESTAMP))) + geom_histogram()
+
+
 #TEMPEST EVENTS
-weeks0_start <- c("2021-05-30", "2021-06-06")
-weeks0_end <- c("2021-06-20", "2021-06-27")
-weeks0_start<- as_date(weeks0_start)
-weeks0_end<- as_date(weeks0_end)
+tempest_events <- bind_rows(
+  tibble( Year = 2021, flood_start = "2021-06-12", flood_end = "2021-06-12"), #average date = June 12
+  tibble( Year = 2022, flood_start = "2022-06-22", flood_end = "2022-06-22"), #June 22
+  tibble( Year = 2023, flood_start = "2023-06-06", flood_end = "2023-06-07"), #June 6, 7
+  tibble( Year = 2024, flood_start = "2024-06-11", flood_end = "2024-06-13")) #June 11, 12, 13
 
-weeks1_start <- c("2022-06-05", "2022-06-12") #June 22
-weeks1_end <- c("2022-06-26", "2022-07-03")
-weeks1_start<- as_date(weeks1_start)
-weeks1_end<- as_date(weeks1_end)
+tempest_events %>%
+  mutate(flood_start = ymd(flood_start),
+         flood_end = ymd(flood_end)) -> events
 
-weeks2_start <- c("2023-05-21", "2023-05-28") #June 6, 7
-weeks2_end <- c("2023-06-11", "2023-06-18")
-weeks2_start<- as_date(weeks2_start)
-weeks2_end<- as_date(weeks2_end)
-
-weeks3_start <- c("2024-05-26", "2024-06-02") #june 11, 12, 13
-weeks3_end <- c("2024-06-16", "2024-06-23")
-weeks3_start<- as_date(weeks3_start)
-weeks3_end<- as_date(weeks3_end)
+#window of data to look at
+window <- days(14)
 
 #Tidy up our data: 
 full_data %>%
   dplyr::select(Year, Species, ID, TIMESTAMP, Plot, `F`) %>%
   mutate(Date = as_date(date(TIMESTAMP)),
          Hour = hour(TIMESTAMP)) %>%
-  filter(Hour <= 13, Hour >= 10, 
+  left_join(events) %>%
+  group_by(Year) %>%
+  mutate(data_start = flood_start - window,
+         data_end = flood_end + window) %>%
+  filter(Date > data_start,
+         Date < data_end,
+         Hour < 14, Hour >= 11, 
          Plot != "Freshwater",
-         Species == "Tulip Poplar",
-         month(TIMESTAMP) %in% c(5, 6, 7)) -> summer_tulip_salt
-
-summer_tulip_salt %>%
-  filter(Year == 2021,
-         Date >= weeks0_start & Date <= weeks0_end) %>%
-  mutate(BA = "Before",
-         Day = as.numeric(Date - as_date("2021-06-16"))) -> year_zero
-
-summer_tulip_salt %>%
-  filter(Year == 2022,
-         Date >= weeks1_start & Date <= weeks1_end) %>%
-  mutate(BA = ifelse(Date < "2022-06-22", "Before", "After"),
-         Day = as.numeric(Date - as_date("2022-06-22"))) -> year_one
-
-summer_tulip_salt %>%
-  filter(Year == 2023,
-         Date >= weeks2_start & Date <= weeks2_end) %>%
-  mutate(BA = ifelse(Date < "2023-06-07", "Before", "After"),
-         Day = as.numeric(Date - as_date("2023-06-07"))) -> year_two
-
-#doesn't work because the data aren't there
-# summer_tulip_salt %>%
-#   filter(Year == 2024,
-#          Date >= weeks3_start & Date <= weeks3_end) %>%
-#   mutate(BA = ifelse(Date < "2024-06-13", "Before", "After"),
-#          Day = as.numeric(Date - as_date("2024-06-13"))) -> year_three
-
-year_zero %>%
-  bind_rows(year_one) %>%
-  bind_rows(year_two) -> tmp_baci
+         Species == "Tulip Poplar") %>%
+  ungroup() -> tulip_salt
 
 #[tmp_baci$`F` > 6e-06,]
 ggplot(tmp_baci[tmp_baci$`F` < 6e-06,], aes(Day, `F`, color = ID)) +
   geom_point() + facet_grid(Plot~Year, scales = "free")
 
-#Obtain average F for 11 am - 12 pm period: 
-tmp_baci2 <- tmp_baci %>%
-  mutate(Year_F = as.factor(tmp_baci$Year),
-         ID_F = as.factor(tmp_baci$ID),
-         BA_F = as.factor(tmp_baci$BA),
-         Plot_F = as.factor(tmp_baci$Plot)) %>%
+#Obtain average F for 11 am - 2 pm period: 
+tulip_salt_baci <- tulip_salt %>%
+  mutate(BA = ifelse(Date < flood_start, "Before", "After"),
+         Year = as.factor(tulip_salt$Year),
+         ID = as.factor(tulip_salt$ID),
+         BA = as.factor(BA),
+         Plot = as.factor(tulip_salt$Plot)) %>%
   filter(`F` < 6e-06) %>%
-  group_by(Date, Plot_F, ID_F, Year_F, Day, BA_F) %>%
+  group_by(Date, Plot, ID, Year, BA) %>%
   summarise(F_avg = mean(`F`, na.rm = TRUE),
             n = length(.))
- 
-ggplot(tmp_baci2, aes(Day, F_avg, color = ID_F)) +
-  geom_point() + facet_grid(Plot_F~Year_F, scales = "free")
 
-ggplot(tmp_baci2, aes(Day, F_avg, color = ID_F, shape = BA_F)) +
-  geom_point() + facet_grid(Plot_F~Year_F, scales = "free")
+tulip_salt_baci %>%
+  arrange(ID, Year) -> test
 
-#Ignore everything after this!! 
-#Testing the following model: 
-# sapflow ~ BA + site + BA*site + (1 | TreeID)
-
+ggplot(tmp_baci2, aes(Day, F_avg, color = ID, shape = BA)) +
+  geom_point(size = 2.5) + facet_grid(Plot~Year, scales = "free")
 
 
 # Fit model without the interaction of random effects (Site and Year)
-#Cand.set <- list()
+Cand.set <- list()
 
-#Cand.set[[1]] 
-glmm_tmp2 <- glmer(F_avg ~ BA_F + Plot_F + BA_F*Plot_F +
-                         (1|ID_F) + (1|Year_F) +
-                         (1|ID_F:1|Year_F),
-                  data = tmp_baci2, family = gaussian)
+#model.noint
+Cand.set[[1]] <- glmer(sqrt(F_avg) ~ BA + Plot  + BA*Plot +
+                         (1|ID) + (1|Year),
+                       data = tmp_baci2, family = gaussian)
+#model.int 
+Cand.set[[2]] <- glmer(sqrt(F_avg) ~ BA + Plot + BA*Plot +
+                         (1|ID) + (1|Year) +
+                         (1|ID:Year),
+                       data = tmp_baci2, family = gaussian)
 
-#Cand.set[[2]]
-model.noint <- glmer(F_avg ~ BA_F + Plot_F  + BA_F*Plot_F +
-                         (1|ID_F) + (1|Year_F),
-                     data = tmp_baci2, family = gaussian)
 
 
 AIC.res.table <- aictab(cand.set = list(Cand.set[[1]], Cand.set[[2]]), 
                         modnames = paste0("Cand.set_", c(1,2)), 
                         second.ord = TRUE)
+AIC.res.table
 
-anova(glmm_tmp2, model.noint)
+# Model selection based on AICc:
+#   
+#   K      AICc Delta_AICc AICcWt Cum.Wt   Res.LL
+# Cand.set_2 8 -26340.29       0.00      1      1 13178.22
+# Cand.set_1 7 -26142.40     197.89      0      1 13078.26
 
+#Suggests that the second model, with the interaction of ID and year
+#is the better match for our data
 
+#dispersion control? not necessary for normally distributed data
 
+#check assumptions of normality
+qqnorm(residuals(model.int), 
+       main = "Q-Q plot - residuals")
+qqline(residuals(model.int), col="red")
 
+# inspecting the random effects (see also Bolker, 2009 - supp 1)
+qqnorm(unlist(ranef(model.int)), 
+       main = "Q-Q plot, random effects")
+qqline(unlist(ranef(model.int)), col="red")
+
+# fitted vs residuals
+scatter.smooth(fitted(model.int), 
+               residuals(model.int, type="pearson"),
+               main="fitted vs residuals",
+               xlab="Fitted Values", ylab="Residuals")
+abline(h=0, col="red")
+
+#definitely a little right skewed
+
+library(e1071)
+current_skewness <- skewness(tmp_baci2$F_avg)
+print(paste("Current Skewness:", current_skewness))
+
+# Logarithmic Transformation
+log_transformed_response <- log(tmp_baci2$F_avg + 1)
+
+# Square Root Transformation
+sqrt_transformed_response <- sqrt(tmp_baci2$F_avg)
+
+# Inverse Transformation
+inv_transformed_response <- 1 / tmp_baci2$F_avg
+
+log_skewness <- skewness(log_transformed_response)
+sqrt_skewness <- skewness(sqrt_transformed_response)
+inv_skewness <- skewness(inv_transformed_response)
+
+print(paste("Log Transformation Skewness:", log_skewness))
+print(paste("Square Root Transformation Skewness:", sqrt_skewness))
+print(paste("Inverse Transformation Skewness:", inv_skewness))
+
+ggplot(tmp_baci2, aes(sqrt(F_avg))) + geom_histogram() +
+  facet_grid(Plot~.)
+
+# not sure what this does - ignore for now
+  # fitted vs observed
+  # scatter.smooth(fitted(model.int), BACI.dt$PropCL,
+  #                xlab="Fitted Values", ylab="Observed PropCL")
+  # abline(0,1, col="red")
+
+####
+####
+####
 refdist.pb.100.interaction <- PBrefdist(largeModel = glmm_tmp, 
                                         smallModel = model.noint, 
                                         nsim = 100, seed = 1989)
--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 
 set.seed(123)
 nochange <- function() rnorm(50, mean = 1, sd = 0.25)
-change <- function() rnorm(50, mean = 1.25, sd = 0.25)
+change <- function() rnorm(50, mean = 1.15, sd = 0.25)
 
 library(dplyr)
 library(car)
@@ -11,15 +11,16 @@ baci <- bind_rows(
   tibble(case = "impact_B", year = 1951:2000, value = nochange()),
   tibble(case = "control_A", year = 2001:2050, value = nochange()),
   #tibble(case = "+t", x_control = nochange(), x_treatment = change()),
-  tibble(case = "impact_A", year = 2001:2050, value = change())
+  tibble(case = "impact_A", year = 2001:2050, value = nochange())
 )
 
 baci %>%
   separate(case, into = c("CI", "BA"), sep = "_") -> baci2
 
 
-ggplot(baci2, aes(year, value, color = CI, shape = BA)) + geom_point() 
+ggplot(baci2, aes(year, value, color = CI, shape = BA)) + geom_point(size = 3) 
 
 test_baci <- glmer(value ~ BA + CI + BA*CI + (1|year),
                    data = baci2, family = gaussian)
+
 Anova(test_baci, type = "III")  
