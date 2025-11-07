7142ecaf309894e5208592a2ec4c91d0d3365e5e
Kendalynn Morris <89651444+kendalynnm@users.noreply.github.com>
2025-11-07 13:46:41 -0500

    Update scratch2.R

--- scratch2.R
+++ scratch2.R
@@ -33,15 +33,40 @@ sapflow_2_long %>%
        y = "Species Avg, Trt - Control") +  # Labels for the plot
   theme_minimal()  # Minimal theme
 
-ec_15clean %>% 
+data %>% 
+  dplyr::select(TIMESTAMP, ec_avg, ec_sd, Plot, Year) %>%
   mutate(Hour = hour(TIMESTAMP),
          Date = date(TIMESTAMP)) %>%
-  filter(Hour >= 11, Hour <= 12) %>% 
-  group_by(Plot, Species, Date) %>% 
-  na.omit() -> ec_plot_avg
+  filter(Hour == 11) %>%
+  distinct() -> ec_data
 
-ec_plot_avg %>%
-  ggplot(aes(x = Date, y = ec_avg, color = Plot)) +
-  geom_point(size = 2.5) +
+
+ec_data %>%
+  group_by(Plot) %>%
+  arrange(Date) %>%
+  mutate(ec_roll = rollmean(ec_avg, k = 4, fill = NA, align = "left")) %>%
+  # above computes rolling mean
+  ungroup() ->ec_rollmean
+
+ec_data %>%
+  group_by(Plot) %>%
+  sample_frac(0.2) -> ec_sub
+
+ec_sub %>%
+ggplot(aes(x = Date, y = ec_avg, color = Plot, group = Plot)) +
+  geom_point(shape = 1) +
+  geom_errorbar(aes(ymin = ec_avg-ec_sd, ymax = ec_avg+ec_sd,
+                    color = Plot, group = Plot), alpha = 0.2) +
+  geom_line(data = ec_rollmean, aes(x = Date, y = ec_roll,
+                             color = Plot, group = Plot)) +
   scale_color_brewer(palette = "Dark2") +
-  theme_minimal()  # Minimal theme
+  theme_minimal()
+
+
+ec_sub %>%
+  ggplot(aes(x = Date, y = log(ec_avg+10), color = Plot, group = Plot)) +
+  geom_point(shape = 1) +
+  geom_errorbar(aes(ymin = log(ec_avg-ec_sd+10), ymax = log(ec_avg+ec_sd+10),
+                    color = Plot, group = Plot), alpha = 0.2) +
+  scale_color_brewer(palette = "Dark2") +
+  theme_minimal()
