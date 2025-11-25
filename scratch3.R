5fd1448ea1a3337af5722d7d11ddd9a615b5ca3e
Kendalynn Morris <89651444+kendalynnm@users.noreply.github.com>
2025-11-25 15:23:45 -0500

    incorporating scratch3 into markdown

--- scratch3.R
+++ scratch3.R
@@ -10,19 +10,20 @@ data %>%
   dplyr::select(Year, Species, ID, TIMESTAMP, Plot, `F`,
                 ec_avg, swc_avg, TEMP, PAR) %>%
   mutate(Date = as_date(date(TIMESTAMP)),
+         doy = yday(Date),
          Hour = hour(TIMESTAMP)) %>%
   group_by(Year) %>%
-  filter(month(Date) == 6, #just June
+  filter(doy > 80 & doy < 320, #Peter's growing season
          #should add more data once available
-         Hour <= 14, Hour >= 11, #same window as for main markdown
-         F <= 0.005, F > 0) %>% #same filtering as for main markdown
+         Hour <= 14, Hour > 11, #same window as for main markdown
+         F <= 0.008, F > 0) %>% #same filtering as for main markdown
   ungroup() -> BACI_data
 
 BACI_data %>%
   mutate(Year = as.factor(Year),
          ID = as.factor(ID),
          Plot = as.factor(Plot)) %>%
-  group_by(Date, Plot, Species, ID, Year) %>%
+  group_by(doy, Date, Plot, Species, ID, Year) %>%
   summarise(F_avg = mean(`F`, na.rm = TRUE),
             soil_ec_avg = mean(ec_avg), 
             soil_vwc_avg = mean(swc_avg),
@@ -30,13 +31,46 @@ BACI_data %>%
             temp_avg = mean(TEMP),
             n = n()) %>%
   ungroup() %>%
-  filter(! Year %in% c(2022, 2023, 2024),
+  filter(#! Year %in% c(2022, 2023, 2024),
          Species %in% c("Beech", "Tulip Poplar", "Red Maple")) -> avg_BACI_data
+
+
+# List of species and plots
+species_list <- c("Red Maple", "Tulip Poplar", "Beech")
+plot_list <- c("Control", "Freshwater", "Saltwater")
+
+# Loop through each species and plot combination to generate plots
+for (species in species_list) {
+  for (plot in plot_list) {
+    avg_BACI_data %>%
+      filter(Plot == plot, Species == species) %>%
+      ggplot(aes(x = doy, y = F_avg, color = Year)) +
+      geom_point() +
+      facet_wrap(. ~ ID, ncol = 3, scales = "fixed") +
+      scale_color_viridis_d(option = 'D', begin = .25, end = .8) +
+      theme_light() +
+      labs(
+        y = "Midday Sap Flux Density cm3/cm2/s",
+        x = "Day of Year",
+        title = paste(" Averaged Sap Flux Density,", species, "-", plot)
+      ) -> p
+    
+    print(p)
+  }
+}
+
+#based on these plots excluding the following trees:
+# C8, C19
+
+avg_BACI_data %>%
+  filter(! Year %in% c(2022, 2023, 2024),
+         ! ID %in% c("C8", "C19"),
+         month(Date) == 6) -> final_BACI_data
 
 #base model, here Year is acting as "BA", and we're allowing all levels of plot and species to interact
 #random effect of ID allows for different trees to have different intercepts
 model_3way <- lmer(F_avg ~ Year*Plot*Species + (1|ID),
-                   data = avg_BACI_data)
+                   data = final_BACI_data)
 #calculate estimated marginal means
 model_emm <- emmeans(model_3way, ~ Species|Plot*Year)
 emm_3way <- as.data.frame(model_emm)
@@ -70,8 +104,18 @@ ggplot(emm_3way, aes(x = Year, y = emmean, color = Plot)) +
 
 avg_BACI_data %>%
   filter(Year == 2025,
+         month(Date) == 6,
          Species == 'Tulip Poplar') -> LITU_data
 
 LITUmodel <- aov(F_avg ~ Plot,
                    data = LITU_data)
 TukeyHSD(LITUmodel)
+
+avg_BACI_data %>%
+  filter(Species == "Beech",
+         Plot == "Control",
+         month(Date) == 6) -> FAGR_data
+
+FAGRmodel <- aov(F_avg ~ Year,
+                 data = FAGR_data)
+TukeyHSD(FAGRmodel)
