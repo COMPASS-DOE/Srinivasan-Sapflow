c42c0afc245273ba89e668351827c1f897e7d9de
Kendalynn Morris <89651444+kendalynnm@users.noreply.github.com>
2025-12-08 08:56:39 -0500

    pausing

--- ec_plot.R
+++ ec_plot.R
@@ -1,129 +1,92 @@
-library(emmeans)
 
 data %>%
-  dplyr::select(Year, Species, ID, TIMESTAMP, Plot, `F`, Fd,
-                `soil-vwc-15cm`, `soil-ec-15cm`) %>%
+  dplyr::select(Year, TIMESTAMP, Plot,
+                swc_avg, swc_sd, ec_avg, ec_sd) %>%
   mutate(Date = as_date(date(TIMESTAMP)),
          Hour = hour(TIMESTAMP)) %>%
-  left_join(events) %>%
-  group_by(Year) %>%
-  mutate(data_start = flood_start - window,
-         data_end = flood_end + window) %>%
-  filter(Date > data_start & Date < flood_start |
-           Date < data_end & Date > flood_end,
-         Hour <= 14, Hour >= 11,
-         F <= 0.005, F >= 0) %>%
-  ungroup() -> sapflow
+  filter(Hour <= 14, Hour >= 11) %>%
+  na.omit() %>%
+  group_by(Year, Date, Plot) %>%
+  summarise(soil_ec_avg = mean(ec_avg),
+            ec_error = sd(ec_avg),
+            soil_vwc_avg = mean(swc_avg),
+            vwc_error = sd(swc_avg)) -> soil_data
 
-sapflow %>%
-  group_by(Year) %>%
-  mutate(BA = ifelse(Date > flood_end, "After", "Before"),
-         Year = as.factor(Year),
-         ID = as.factor(ID),
-         BA = as.factor(BA),
-         Plot = as.factor(Plot)) %>%
-  group_by(Date, Plot, Year, Species, BA, flood_start, flood_end) %>%
-  summarise(F_avg = mean(`F`, na.rm = TRUE),
-            n = n()) %>%
-  ungroup() -> sapflow_1
+ggplot(soil_data, aes(Date, soil_ec_avg, color = Plot)) +
+  geom_point() + #facet_wrap(~Year, scales = "free") +
+  geom_errorbar(aes(ymin = soil_ec_avg - ec_error, ymax = soil_ec_avg + ec_error))
 
-sapflow_1 %>%
-  group_by(Year) %>%
-  mutate(Day = ifelse(BA == "Before",
-                      - as.numeric(flood_start - Date),
-                      as.numeric(Date - flood_end))) %>%
-  mutate(BA = as.character(BA)) %>%
-  na.omit() -> sapflow_2 #%>%
-  # mutate(BA = ifelse(Year == 2021, "Before", BA),
-  #        BA = ifelse(Year == 2025, "After", BA)) -> tsb_2
+ggplot(soil_data, aes(Date, soil_vwc_avg, color = Plot)) +
+  geom_point() + #facet_wrap(~Year, scales = "free") +
+  geom_errorbar(aes(ymin = soil_vwc_avg - vwc_error, ymax = soil_vwc_avg + vwc_error))
 
-ggplot(sapflow_2, aes(Day, F_avg, color = Species, shape = BA)) +
-  geom_point(size = 2.5) + facet_grid(Year~Plot, scales = "free")
+soil_data %>%
+  filter(! Year %in% c(2022, 2023, 2024),
+         month(Date) == 6) -> soil_BACI_data
 
-sapflow_2_long <- sapflow_2 %>%
-  group_by(Year, Day, BA, Plot, Species) %>% 
-  summarize(Species_avg = mean(F_avg, na.rm = TRUE), .groups = "drop") %>% 
-  pivot_wider(names_from = Plot, values_from = Species_avg) %>% 
-  mutate(Freshwater_Control = Freshwater - Control,
-         Saltwater_Control = Saltwater - Control) %>%
-  pivot_longer(cols = starts_with(c("Freshwater_Control", "Saltwater_Control")),
-               names_to = "Plot_Normalized",
-               values_to = "Normalized_avg")
+#base model, here Year is acting as "BA", and we're allowing all levels of plot and species to interact
+ec_2way <- aov(soil_ec_avg ~ Year*Plot,
+                   data = soil_BACI_data)
+#calculate estimated marginal means
+ec_emm <- emmeans(ec_2way, ~ Plot|Year)
+emm_2way <- as.data.frame(ec_emm)
+#shows pairwise tests of 2021 vs 2025 data for each species x plot group
+contrasts <- contrast(ec_emm, interaction = "pairwise")
+summary(contrasts)
 
-ggplot(sapflow_2_long, aes(Day, Normalized_avg, color = Species, shape = BA)) +
-  geom_point(size = 2.5) + facet_grid(Plot_Normalized~Year) +
-  geom_hline(yintercept = 0, color = 'red')
+#setting a width for offsets below
+dodge_width <- 0.25
 
-
-
-data %>%
-  dplyr::select(Year, Species, ID, TIMESTAMP, Plot, `F`, Fd,
-                `soil-vwc-15cm`, `soil-ec-15cm`) %>%
-  mutate(Date = as_date(date(TIMESTAMP)),
-         Hour = hour(TIMESTAMP)) %>%
-  filter(Year == 2024,
-         month(Date) == 4,
-         day(Date) >= 15, day(Date) <= 30,  # Last two weeks of April
-         Hour <= 14, Hour >= 11,
-         F <= 0.005, F >= 0
-         ) %>%
-  group_by(Date, Plot, Species, ID) %>%
-  summarise(F_avg = mean(`F`, na.rm = TRUE),
-            n = n()) %>%
-  ungroup() -> anova
-
-simple <- aov(F_avg ~ Species*Plot, data = anova)
-emm <- emmeans(simple, ~ Plot|Species)
-pairwise_comparisons <- pairs(emm, adjust = "tukey")
-summary(pairwise_comparisons)
-
-# Extract estimated marginal means
-emm_data <- as.data.frame(emm)
-
-ggplot(emm_data, aes(x = Plot, y = emmean, color = Species, group = Species)) +
-  geom_point(size = 3) +  # Points for estimated marginal means
-  geom_line(linewidth = 1) +   # Lines to connect species across plots
-  geom_errorbar(aes(ymin = emmean - SE, ymax = emmean + SE), width = 0.25) +  # Error bars
-  labs(title = "Historical Recreation of Radha's Initial Anova",
-       x = "Plot",
-       y = "Estimated Marginal Mean of F_avg") +
-  theme_minimal() + scale_color_brewer(palette = "Dark2") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-
-
-
-
-data %>%
-  dplyr::select(Year, Species, ID, TIMESTAMP, Plot, `F`, Fd,
-                `soil-vwc-15cm`, `soil-ec-15cm`) %>%
-  mutate(Date = as_date(date(TIMESTAMP)),
-         Hour = hour(TIMESTAMP)) %>%
-  filter(Year == 2025,
-         month(Date) == 6,
-         Hour <= 14, Hour >= 11,
-         F <= 0.005, F >= 0
-  ) %>%
-  group_by(Date, Plot, Species, ID) %>%
-  summarise(F_avg = mean(`F`, na.rm = TRUE),
-            n = n()) %>%
-  ungroup() -> anova2
-
-simple2 <- aov(F_avg ~ Species*Plot, data = anova2)
-emm2 <- emmeans(simple2, ~ Plot|Species)
-pairwise_comparisons2 <- pairs(emm2, adjust = "tukey")
-summary(pairwise_comparisons2)
-
-# Extract estimated marginal means
-emm_data2 <- as.data.frame(emm2)
-
-ggplot(emm_data2, aes(x = Plot, y = emmean, color = Species, group = Species)) +
-  geom_point(size = 3) +  # Points for estimated marginal means
-  geom_line(linewidth = 1) +   # Lines to connect species across plots
-  geom_errorbar(aes(ymin = emmean - SE, ymax = emmean + SE), width = 0.25) +  # Error bars
-  labs(title = "Interaction Between Species and Plot",
-       x = "Plot",
-       y = "Estimated Marginal Mean of F_avg") +
+#plot of 21 vs 25 EMM with 95% CI
+ggplot(emm_2way, aes(x = Year, y = emmean, color = Plot)) +
+  geom_point(size = 3, position = position_dodge(width = dodge_width)) +
+  geom_errorbar(
+    aes(ymin = lower.CL, ymax = upper.CL),
+    width = 0.25,  
+    position = position_dodge(width = dodge_width)) +
+  geom_line(
+    aes(group = Plot),
+    position = position_dodge(width = dodge_width),
+    linetype = "dashed") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  labs(
+    y = "Estimated Marginal Mean (Response)",
+    x = "June Soil EC",
+    title = "Estimated Marginal Means, 2021 vs 2025 Soil EC") +
+  theme(
+    strip.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
+#base model, here Year is acting as "BA", and we're allowing all levels of plot and species to interact
+vwc_2way <- aov(soil_vwc_avg ~ Year*Plot,
+               data = soil_BACI_data)
+#calculate estimated marginal means
+vwc_emm <- emmeans(vwc_2way, ~ Plot|Year)
+emm_2way <- as.data.frame(vwc_emm)
+#shows pairwise tests of 2021 vs 2025 data for each species x plot group
+contrasts <- contrast(vwc_emm, interaction = "pairwise")
+summary(contrasts)
+
+#setting a width for offsets below
+dodge_width <- 0.25
+
+#plot of 21 vs 25 EMM with 95% CI
+ggplot(emm_2way, aes(x = Year, y = emmean, color = Plot)) +
+  geom_point(size = 3, position = position_dodge(width = dodge_width)) +
+  geom_errorbar(
+    aes(ymin = lower.CL, ymax = upper.CL),
+    width = 0.25,  
+    position = position_dodge(width = dodge_width)) +
+  geom_line(
+    aes(group = Plot),
+    position = position_dodge(width = dodge_width),
+    linetype = "dashed") +
+  theme_minimal() +
+  labs(
+    y = "Estimated Marginal Mean (Response)",
+    x = "June Soil VWC",
+    title = "Estimated Marginal Means, 2021 vs 2025 Soil VWC") +
+  theme(
+    strip.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"))
+
